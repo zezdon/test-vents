4fcc7f1be65d03e03c67f6c0446a090970be2405
Mats G <ed54res@hotmail.com>
2020-09-24 16:22:27 +0200

    Assets files and NavBar

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar';
+import { Container } from 'semantic-ui-react';
 
 function App() {
   return (
-    <div>
-      <h1>Test-vents</h1>
-      <EventDashboard />
-    </div>
+    <>
+      <NavBar />
+      <Container className='main'>
+        <EventDashboard />
+      </Container>      
+    </>
   );
 }
 
