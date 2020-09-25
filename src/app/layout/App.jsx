88c3021d169cf63777725b394260601eb023e7e1
Mats G <ed54res@hotmail.com>
2020-09-26 00:03:54 +0200

    Router NavBar

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -2,6 +2,10 @@ import React, {useState} from 'react';
 import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar';
 import { Container } from 'semantic-ui-react';
+import { Route } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
+import EventDetailedPage from '../../features/events/eventDetailed/EventDetailedPage';
+import EventForm from '../../features/events/eventForm/EventForm';
 
 function App() {
   const [formOpen, setFormOpen] = useState(false);
@@ -21,12 +25,10 @@ function App() {
     <>
       <NavBar setFormOpen={handleCreateFormOpen}/>
       <Container className='main'>
-        <EventDashboard 
-          formOpen={formOpen} 
-          setFormOpen={setFormOpen} 
-          selectEvent={handelSelectEvent}
-          selectedEvent={selectedEvent}
-        />
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/events' component={EventDashboard} />
+        <Route path='/events/:id' component={EventDetailedPage} />
+        <Route path='/createEvent' component={EventForm} />
       </Container>      
     </>
   );
