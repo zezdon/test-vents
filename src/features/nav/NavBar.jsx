e64c650d8b621e6f8f4125f4d2d32ef8e714f948
Mats G <ed54res@hotmail.com>
2020-09-26 16:50:18 +0200

    Adding menus for fake authenticated and unauthenticated users and using useHistory hook

--- src/features/nav/NavBar.jsx
+++ src/features/nav/NavBar.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Menu, Container, Button } from 'semantic-ui-react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
+import SignedOutMenu from './SignedOutMenu';
+import SignedInMenu from './SignedInMenu';
 
 export default function NavBar({setFormOpen}) {
+    const history = useHistory();
+    const [authenticated, setAuthenticated] = useState(false);
+
+    function handleSignOut() {
+        setAuthenticated(false);
+        history.push('/');
+    }
+
     return (
         <Menu inverted fixed='top'>
             <Container>
@@ -11,13 +21,15 @@ export default function NavBar({setFormOpen}) {
                     Test-vents
                 </Menu.Item>
                 <Menu.Item as={NavLink} to='/events' name='Events' />
+                {authenticated &&
                 <Menu.Item as={NavLink} to='/createEvent'>
                     <Button positive inverted content='Create Event' />
-                </Menu.Item>
-                <Menu.Item position='right'>
-                    <Button basic inverted content='Login' />
-                    <Button basic inverted content='Register' style={{marginLeft: '0.5em'}}/>                    
-                </Menu.Item>
+                </Menu.Item>}
+                {authenticated ? (
+                    <SignedInMenu signOut={handleSignOut}/> 
+                ) : ( 
+                    <SignedOutMenu setAuthenticated={setAuthenticated} />
+                )}                
             </Container>
         </Menu>
     )
