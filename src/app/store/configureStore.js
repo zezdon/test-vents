7053efc764c966276f03311a8600943f7a3dd01e
Mats G <ed54res@hotmail.com>
2021-05-12 14:52:51 +0200

    eventActions eventConstants eventReducer remove testReducer

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,7 +1,8 @@
 import {createStore} from 'redux';
-import testReducer from '../../features/sandbox/testReducer';
+import {devToolsEnhancer} from 'redux-devtools-extension';
+import rootReducer from './rootReducer';
 
 export function configureStore() {
 
-    return createStore(testReducer)
+    return createStore(rootReducer, devToolsEnhancer())
 }
