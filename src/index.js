7e2fc91c16db858015a23665bcfad77f1ff5307e
Mats G <ed54res@hotmail.com>
2021-05-12 18:19:42 +0200

    ScrollToTop (dont work Yet)

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from './app/store/configureStore';
+import ScrollToTop from './app/layout/ScrollToTop';
 
 const store = configureStore()
 
@@ -18,6 +19,7 @@ function render() {
   ReactDOM.render(
     <Provider store={store}>
       <BrowserRouter>
+        <ScrollToTop />
         <App />
       </BrowserRouter>
     </Provider>,
