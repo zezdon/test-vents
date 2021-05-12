5ffc9db4b108379fe308dd42abbb26a0d82d7892
Mats G <ed54res@hotmail.com>
2021-05-12 16:26:45 +0200

    use useSelector EventDetailed and Attendees Property

--- src/features/events/eventDetailed/EventDetailedSidebar.jsx
+++ src/features/events/eventDetailed/EventDetailedSidebar.jsx
@@ -1,39 +1,40 @@
 import React from 'react';
-import { Segment, Item } from 'semantic-ui-react';
+import { Segment, Item, Label } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
-export default function EventDetailedSidebar() {
-    return (
-        <>
-            <Segment
-                textAlign="center"
-                style={{border: 'none'}}
-                attached="top"
-                secondary
-                inverted
-                color="teal"
-            >
-                2 People Going
-            </Segment>
-            <Segment attached>
-                <Item.Group relaxed divided>
-                    <Item style={{position: 'relative'}}>
-                        <Item.Image size="tiny" src='/assets/user.png'/>
-                        <Item.Content verticalAlign="middle">
-                            <Item.Header as="h3">
-                                <span>Tom</span>
-                            </Item.Header>
-                        </Item.Content>
-                    </Item>
-                    <Item style={{position: 'relative'}}>
-                        <Item.Image size="tiny" src='/assets/user.png'/>
-                        <Item.Content verticalAlign="middle">
-                            <Item.Header as="h3">
-                                <span>Bob</span>
-                            </Item.Header>
-                        </Item.Content>
-                    </Item>
-                </Item.Group>
-            </Segment>
-        </>        
-    )
+export default function EventDetailedSidebar({ attendees, hostUid }) {
+  return (
+    <>
+      <Segment
+        textAlign='center'
+        style={{ border: 'none' }}
+        attached='top'
+        secondary
+        inverted
+        color='teal'
+      >
+        {attendees.length} {attendees.length > 1 ? 'People' : 'Person'} Going
+      </Segment>
+      <Segment attached>
+        <Item.Group relaxed divided>
+          {attendees.map((attendee) => (
+            <Item as={Link} to={`/profile/${attendee.id}`} key={attendee.id} style={{ position: 'relative' }}>
+              {hostUid === attendee.id && (
+                <Label style={{position: 'absolute'}} color='orange' ribbon='right' content='Host' />
+              )}
+              <Item.Image
+                size='tiny'
+                src={attendee.photoURL || '/assets/user.png'}
+              />
+              <Item.Content verticalAlign='middle'>
+                <Item.Header as='h3'>
+                  <span>{attendee.displayName}</span>
+                </Item.Header>
+              </Item.Content>
+            </Item>
+          ))}
+        </Item.Group>
+      </Segment>
+    </>
+  );
 }
