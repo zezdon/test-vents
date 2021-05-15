f7c29a3fca840963fe422276fb0cfbe22523040e
Mats G <ed54res@hotmail.com>
2021-05-15 16:54:07 +0200

    Creating common/modals - modalType(TestModal) and modalProps(data)

--- src/features/sandbox/Sandbox.jsx
+++ src/features/sandbox/Sandbox.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Button } from 'semantic-ui-react';
 import { increment, decrement } from './testReducer';
+import { openModal } from '../../app/common/modals/modalReducer';
 
 export default function Sandbox() {
     const dispatch = useDispatch();
@@ -13,6 +14,13 @@ export default function Sandbox() {
             <h3>The data is: {data}</h3>
             <Button onClick={(e) => dispatch(increment(20))} content='Increment' color='green' />
             <Button onClick={(e) => dispatch(decrement(10))} content='Decrement' color='red' />
+            <Button 
+                onClick={(e) => 
+                    dispatch(openModal({modalType: 'TestModal', modalProps: {data}}))
+                } 
+                content='Open Modal' 
+                color='teal' 
+            />
         </>
     )
 }
