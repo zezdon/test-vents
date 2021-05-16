aed91ca9892c79708209f10ff6ac424b47c39371
Mats G <ed54res@hotmail.com>
2021-05-16 22:33:57 +0200

    Redux Thunk 101 -  Isolating the loading indicators in Sandbox

--- src/app/store/rootReducer.js
+++ src/app/store/rootReducer.js
@@ -1,14 +1,16 @@
 import {combineReducers} from 'redux';
-import authReducer from '../../features/auth/authReducer';
 import eventReducer from '../../features/events/eventReducer';
 import testReducer from '../../features/sandbox/testReducer';
 import modalReducer from '../common/modals/modalReducer';
+import authReducer from '../../features/auth/authReducer';
+import asyncReducer from '../async/asyncReducer';
 
 const rootReducer = combineReducers({
     test: testReducer,
     event: eventReducer,
     modals: modalReducer,
-    auth: authReducer
+    auth: authReducer,
+    async: asyncReducer
 })
 
 export default rootReducer;
--- src/features/sandbox/Sandbox.jsx
+++ src/features/sandbox/Sandbox.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Button } from 'semantic-ui-react';
 import { increment, decrement } from './testReducer';
@@ -6,14 +6,34 @@ import { openModal } from '../../app/common/modals/modalReducer';
 
 export default function Sandbox() {
     const dispatch = useDispatch();
+    const [target, setTarget] = useState(null);
     const data = useSelector(state => state.test.data);
+    const { loading } = useSelector((state) => state.async);    
 
     return (
         <>
             <h1>Testing 123</h1>
             <h3>The data is: {data}</h3>
-            <Button onClick={(e) => dispatch(increment(20))} content='Increment' color='green' />
-            <Button onClick={(e) => dispatch(decrement(10))} content='Decrement' color='red' />
+            <Button
+                name='increment'
+                loading={loading && target === 'increment'}
+                onClick={(e) => {
+                dispatch(increment(20));
+                setTarget(e.target.name)
+                }}
+                content='Increment'
+                color='green'
+            />
+            <Button
+                name='decrement'
+                loading={loading && target === 'decrement'}
+                onClick={(e) => {
+                dispatch(decrement(10));
+                setTarget(e.target.name)
+                }}
+                content='Decrement'
+                color='red'
+            />
             <Button 
                 onClick={(e) => 
                     dispatch(openModal({modalType: 'TestModal', modalProps: {data}}))
